f62d44cf67511b26e9714cc267cca9ab74b6b845
Mark-Prime <msmarkieer@gmail.com>
2021-08-19 17:00:21 -0500

    Set up basic listing app

--- App.js
+++ App.js
@@ -1,32 +1,62 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, Button, ScrollView, FlatList } from 'react-native';
 
 export default function App() {
+
+  const [enteredGoal, setEnteredGoal] = useState('');
+  const [courseGoals, setCourseGoals] = useState([]);
+
+  const goalInputHandler = (text) => {
+    setEnteredGoal(text);
+  };
+
+  const addGoalHandler = () => {
+    setCourseGoals(courseGoals => [...courseGoals, { key: 'goal' + Date.now(), value: enteredGoal }]);
+  };
+
   return (
-    <View style={{padding: 50}}>
-      <View style={{
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-      }}>
+    <View style={styles.container}>
+      <View style={styles.inputContainer}>
         <TextInput 
           placeholder="Course Goal" 
-            style={{
-              borderBottomColor: 'black',
-              borderBottomWidth: 1,
-              padding: 5,
-              width: 200,
-            }}/>
-        <Button title="add"/>
+          style={styles.input}
+          onChangeText={goalInputHandler}
+        />
+        <Button title="add" onPress={addGoalHandler}/>
       </View>
-      <View>
-
-      </View>
+      <FlatList data={courseGoals} renderItem={itemData => (
+        <View style={styles.listItem}>
+          <Text>{itemData.item.value}</Text>
+        </View>
+      )}/>
       
       <StatusBar style="auto" />
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    padding: 50,
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  input: {
+    borderBottomColor: 'black',
+    borderBottomWidth: 1,
+    padding: 5,
+    width: 200,
+    flex: 1,
+  },
+  listItem: {
+    padding: 10,
+    marginVertical: 5,
+    backgroundColor: '#ccc',
+    borderColor: 'black',
+    borderWidth: 1,
+  }
+});
